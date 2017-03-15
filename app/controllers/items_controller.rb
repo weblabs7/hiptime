d3d9d2239e276a3355b29ff96844b4254b76cefb
weblabs7 <weblabs@live.com>
2017-03-15 01:40:21 +0000

    added form and item controllers for crud.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
     
     def index
+        @items = Item.all.order("created_at DESC")
     end
     
     def new
@@ -8,6 +9,18 @@ class ItemsController < ApplicationController
     end
     
     def create
+        @item = Item.new(item_params)
+        if @item.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    
+    def item_params
+        params.require(:item).permit(:title, :description)
     end
     
 end
