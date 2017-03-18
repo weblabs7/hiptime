bad22bc705c30c1b800b5e2a23ce767714b2693b
weblabs7 <weblabs@live.com>
2017-03-18 16:54:44 +0000

    added user_id migration belongs to and has many to models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318140033) do
+ActiveRecord::Schema.define(version: 20170318164835) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
